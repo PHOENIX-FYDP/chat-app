54c6298ef65d5084f3b1137eac148ad45cce5a3e
shafay-azeem <shafayazeem12@gmail.com>
2022-09-06 14:31:08 +0500

    updated

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ console.log(process.env.PORT, "env port");
 const server = app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
 const io = require("socket.io")(server, {
-  pingTimeout: 60000,
+  pingTimeout: 1000000,
   cors: {
     origin: "http://localhost:3000",
     // credentials: true,
